ce7669df6b32b7acba0b8107a93bacc10686b18e
Sahitya Modi <sahityamodi0@gmail.com>
2023-12-19 01:48:46 +0530

    Created Navbar UI with logo

---
+++ src/config/index.tsx
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI KITS",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor Picks",
+        href: "#",
+        image: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        image: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        image: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorite Icon Picks",
+        href: "#",
+        image: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        image: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Bestselling Icons",
+        href: "#",
+        image: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];

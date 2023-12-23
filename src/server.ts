6d5a52418b294d9ca6dd917c09a43f83b43b7c6a
Sahitya Modi <sahityamodi0@gmail.com>
2023-12-23 18:28:25 +0530

    Server setup completed

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { getPayloadClient } from "./get-payload";
+
+const app = express();
+const PORT = Number(process.env.port) || 3000;
+
+const start = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
+    },
+  });
+};
+
+start();
